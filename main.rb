222afa2a4628e3599abcd060d6221627d5dff5d2
Priscilla Falcão <98496732+Prisccca@users.noreply.github.com>
2022-03-21 01:05:01 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,16 @@
+#Say Hello, World in multiple ways
+
+#print "Hello Ruby\n"
+#puts "Hello, World!"
+
+#p "Hello Ruby"
+
+#greeting = "Hello Ruby"
+
+#puts greeting
+
+def say_hello (thing)
+  puts thing
+end
+
+say_hello "Hello Ruby "
